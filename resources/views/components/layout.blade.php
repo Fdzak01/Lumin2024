dd239b6d0b33d998f07a633b260bafc4d0cd03be
Fdzak01 <fdzak01@gmail.com>
2024-11-06 09:08:26 +0700

    viewscreatefolder

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,11 +4,15 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Learning - Tailwind</title>
+    <title>Lumin-Web</title>
     <link rel="stylesheet" href="{{ asset('admin/dist/css/style.css') }}">
     <link rel="stylesheet" href="resources/css/style.css">
 </head>
 <body>
-    
+    <div>
+        <ol>
+            <li>{{ $page_name }}</li>
+        </ol>
+    </div>
 </body>
 </html>
